02d343e99e9881ef62b1d66ac4dbd3ea9c595f21
WIN-S2A3EMTVSPT\Администратор <vladpalagin2013@gmail.com>
2023-07-26 17:25:23 +0300

    no message

--- js/cartCounter.js
+++ js/cartCounter.js
@@ -2,42 +2,34 @@ const arrayCarts = document.querySelectorAll('.i-shopping-cart');
 
 const counterCarts = document.querySelector('.counter-carts');
 
-const minusCount = document.querySelector('.i-minus');
-const plusCount = document.querySelector('.i-plus');
+const minusCount = document.querySelectorAll('.i-minus');
+const plusCount = document.querySelectorAll('.i-plus');
 
 let countCarts = 1;
 
-// arrayCarts.forEach(cart => {
-//     console.log('hi')
-//     cart.addEventListener('mouseenter', function() {
-//         elementClick.addEventListener('click', () => {
-//             count += 1;
-//             counter.innerHTML = `<i class="i i-shopping-cart"></i> ${count}`;
-//         });
-//     });
-// })
-
-minusCount.addEventListener('click', () => {
-    const second = minusCount.parentNode.parentNode.querySelector('.countElement');
-    if (countCarts > 0) {
-        countCarts -= 1;
-        second.value = countCarts;
-    }
+minusCount.forEach(mc=> {
+    mc.addEventListener('click', () => {
+        const second = mc.parentNode.parentNode.querySelector('.countElement');
+        if (countCarts > 0) {
+            countCarts -= 1;
+            second.value = countCarts;
+            countCarts = 1;
+        }
+    })
 })
 
-plusCount.addEventListener('click', () => {
-    const second = minusCount.parentNode.parentNode.querySelector('.countElement');
-    countCarts += 1;
-    second.value = countCarts;
+plusCount.forEach(pc => {
+    pc.addEventListener('click', () => {
+        const second = pc.parentNode.parentNode.querySelector('.countElement');
+        countCarts += 1;
+        second.value = countCarts;
+    })
+    countCarts = 1;
 })
 
 arrayCarts.forEach(cart => {
     const second = cart.parentNode.parentNode.querySelector('.countElement');
     cart.addEventListener('click', () => {
-        console.log(second.value)
-        countCarts += parseInt(second.value);
-        console.log(second.value)
-        console.log(countCarts)
         counterCarts.innerHTML = `<i class="i i-shopping-cart"></i> ${countCarts}`;
     })
 })
